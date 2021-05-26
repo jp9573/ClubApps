f660a52fd960e5eb726f37853a97fcf07fb0f154
jp9573 <jp9573@gmail.com>
2021-05-26 21:01:05 +0530

    Edit order portal

--- src/component/Restaurant/OrderPortal/OrderPortal.js
+++ src/component/Restaurant/OrderPortal/OrderPortal.js
@@ -231,7 +231,7 @@ class OrderPortal extends Component {
       currentMenuItem.customization[currentCustomizationIndex];
     const { step, selectionType, options } = customization;
 
-    let totalPrice = 0;
+    let totalPrice = currentMenuItem.price;
     let isButtonDisabled = false;
 
     if (
